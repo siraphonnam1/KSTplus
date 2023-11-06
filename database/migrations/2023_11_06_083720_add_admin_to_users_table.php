e7d24075ef1d797ad66ab903b8f384a25d47b87d
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-11-06 15:40:33 +0700

    Create 2023_11_06_083720_add_admin_to_users_table.php

---
+++ database/migrations/2023_11_06_083720_add_admin_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            ['name' => 'adminKST'],
+            ['username' => 'admin'],
+            ['password' => 'iddrivesadmin'],
+            ['agency' => '0'],
+            ['brn' => '0'],
+            ['dpm' => '0'],
+            ['role' => 'admin'],
+            // ... other rows
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
