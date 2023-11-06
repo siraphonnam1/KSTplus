243776343de47a2a7f4f5c117e263d8849e0de55
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-11-06 16:02:31 +0700

    fx

--- database/migrations/2023_10_31_031901_add_username_to_users_table.php
+++ database/migrations/2023_10_31_031901_add_username_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('email', 'username');
-        });
+        if (!Schema::hasColumn('users', 'email')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->renameColumn('email', 'username');
+            });
+        }
     }
 
     /**
@@ -22,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('username', 'email');
+
         });
     }
 };
