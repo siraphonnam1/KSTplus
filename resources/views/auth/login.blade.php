2d5e0d30a589049d771830ff896f3571c08329dd
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-11-08 15:33:47 +0700

    fx

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="username" :value="__('username')" />
+            <x-input-label for="username" :value="__('Username')" />
             {{-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" /> --}}
             <input type="text" name="username" id="username" class="form-control" required autofocus autocomplete="username">
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
