ba453069a12ac47c454bf10ec955c1d1ebd982ff
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-11-29 11:32:24 +0700

    fx

--- resources/views/page/exports/exportData.blade.php
+++ resources/views/page/exports/exportData.blade.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>KST Export Data</title>
     <link rel="icon" href="/img/logoiddrives.png" type="image/icon type">
 </head>
