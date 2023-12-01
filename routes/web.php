2b65552413ea7a7724a170160d07510a7b4de1e7
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-12-01 09:49:25 +0700

    fx

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::get('/', function () {
 // Route::get('/main', function () {
 //     return view('page.main');
 // })->middleware(['auth', 'verified'])->name('main');
+Route::get('/phpinfo', function () {
+    phpinfo();
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
